18a6d06971240177bdd9d426542dc97f61221338
sarah <52550982+raaamen@users.noreply.github.com>
2020-01-15 23:25:00 -0500

    no message

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public int score;
+
+    public bool press;
+
+    public KeyCode key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (press&&Input.GetKeyDown(key))
+        {
+
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Note")
+        {
+            press = true;
+            if (Input.GetKeyDown(key))
+            {
+                Destroy(collision.gameObject);
+                score++;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Note")
+        {
+            press = false;
+
+        }
+    }
+
+
+}
