c4507392b10c7c692b24d45979c888ce471f13df
Sarah Applebaum <sarahapplebaum.sia@gmail.com>
2020-01-22 12:58:03 -0500

    no message

---
+++ Assets/Scripts/NoteClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteClass : MonoBehaviour
+{
+
+    Vector3 pos;
+
+    public NoteClass(Vector3 v)
+    {
+        pos = v;
+    }
+
+    public Vector3 getPos()
+    {
+        return pos;
+    }
+
+}
---
+++ Assets/Scripts/PatternCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternCreator : MonoBehaviour
+{
+
+    public NoteClass[] noteslvl1;
+    public NoteClass[] noteslvl2;
+    public NoteClass[] noteslvl3;
+
+    public GameObject noteObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadNotes()
+    {
+        foreach (var item in noteslvl1)
+        {
+            Instantiate(noteObj, item.getPos(), Quaternion.identity);
+        }
+    }
+
+}
