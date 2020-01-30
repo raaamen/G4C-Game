ed882e77a8271d40a6eed1160047d8a07484e674
sarah <52550982+raaamen@users.noreply.github.com>
2020-01-30 14:22:03 -0500

    worked on comic script manager

---
+++ Assets/Scripts/ComicManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ComicManager : MonoBehaviour
+{
+    public Image comicPanel;
+    public Sprite[] comicHolder;
+    public int i = 0;
+    public bool comicDone;
+    public GameObject continueButton;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && i < comicHolder.Length)
+        {
+            comicPanel.sprite = comicHolder[i];
+            i++;
+            if (i==comicHolder.Length-1)
+            {
+                continueButton.SetActive(true);
+            }
+
+        }
+    }
+}
